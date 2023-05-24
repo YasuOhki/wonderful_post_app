de4be0467d808aa142013fe980386c38c940c537
Yasuhiro Ohki <>
2023-05-24 22:52:58 +0900

    ・テーブルを再作成(articleからarticlesに変更) ・scaffoldで作成していたarticleを一旦削除し、controller、view、modelをrailsコマンドを使って再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_22_005744) do
+ActiveRecord::Schema.define(version: 2023_05_23_135844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
