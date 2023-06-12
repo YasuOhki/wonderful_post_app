68a3b6c972abbc9957e92e8c1870faa8224d6a89
Yasuhiro Ohki <>
2023-06-12 23:32:49 +0900

    article側にもuserと紐づける処理を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence: true, uniqueness: {case_sensitive: false}
   validates :content, presence: true, uniqueness: {case_sensitive: false}
+
+  belongs_to :user
 end
