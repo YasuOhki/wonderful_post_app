237f58fde706ee79c5e2b5c18afd3e67902a35ff
Yasuhiro Ohki <>
2023-07-03 00:17:05 +0900

    Task.14の続き

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,7 +1,17 @@
 class MypageController < ApplicationController
+  before_action :set_search_article, only:  %i[ show ]
 
   # GET /mypage
   def show
-    @articles = current_user.articles.page(params[:page])
+    @articles = @articles.page(params[:page])
   end
 end
+
+private
+  def set_search_article
+    if params[:title] == nil
+      @articles = current_user.articles
+    else
+      @articles = current_user.articles.where("title LIKE ?", "%#{params[:title]}%")
+    end
+  end
