09c27610f4b19552249903378f357b81fe6aa75a
Yasuhiro Ohki <>
2023-07-04 23:39:09 +0900

    Task.15-2の修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: {case_sensitive: false}
 
-  has_many :article
-  has_many :article, through: :tag_article
+  has_many :tag_articles, dependent: :destroy
+  has_many :articles, through: :tag_article
 end
